150be67c7ed14569b48964a45c6b7df641a522f0
anwesh <pandaanwesh33@gmail.com>
2022-07-11 12:55:45 +0530

    dynamic ports added

--- app.js
+++ app.js
@@ -51,6 +51,10 @@ app.use(commentRoutes);
 app.use(campgroundRoutes);
 
 
-app.listen(5000,'0.0.0.0',()=>{
-    console.log("server has started");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// app.listen(5000,'0.0.0.0',()=>{
+//     console.log("server has started");
+// });
