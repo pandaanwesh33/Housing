c68f7461fdbbf14f95bcf106899fd5c812a76896
anwesh <pandaanwesh33@gmail.com>
2022-08-30 02:03:40 +0530

    changed texts

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
-// app.listen(5000,'0.0.0.0',()=>{
+// app.listen(3000,'0.0.0.0',()=>{
 //     console.log("server has started");
 // });
