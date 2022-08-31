f9b46ee7d4c1c432e9081125e0a8d3b7122aa273
anwesh <pandaanwesh33@gmail.com>
2022-08-31 18:18:15 +0530

    campgrounds routes updated

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -65,32 +65,32 @@ router.get("/houses/:id",function(req,res){
 
 
 //EDIT CAMPGROUNDS
-router.get("/campgrounds/:id/edit", middleware.checkCampgroundOwnership, function(req,res){
+router.get("/houses/:id/edit", middleware.checkCampgroundOwnership, function(req,res){
     Campground.findById(req.params.id, function(err,foundCampground){
         res.render("campgrounds/edit", {campground: foundCampground});
     });
 });
 
 //UPDATE CAMPGROUNDS
-router.put("/campgrounds/:id", middleware.checkCampgroundOwnership, function(req,res){
+router.put("/houses/:id", middleware.checkCampgroundOwnership, function(req,res){
     //find ahttps://www.google.com/search?q=house&sxsrf=ALiCzsbhBA8AAbjwOZCQr79hN5ZI0J88vw:1656416105857&tbm=isch&source=iu&ictx=1&vet=1&fir=PCj5VWGURIThbM%252Cw_edFuvJNI2ApM%252C_%253B9qAvQPZ8JzcJqM%252CxCIuxCOW5lAuIM%252C_%253BzZX_6N9gJAnIFM%252Cycr1f-MM1FMK6M%252C_%253B9xns9Iqc3cNNsM%252CeJvVnNA6DtNy7M%252C_%253B6iqw6QtsXQQNqM%252CY9vB55R2tNGDmM%252C_%253Btlxt7ZlcQKIDTM%252CcxH1oOA7j7DoOM%252C_%253BTYbhjTZTg4cY3M%252CW1XZb07kUNqdBM%252C_%253BH8QuGQ6oGLUmwM%252CuzjYTWgExXvPCM%252C_%253B0uttkJggaCROfM%252CXK8DUNClm8MVmM%252C_%253BkLhGdOQP1hrl2M%252CsRMoSCUWF_uqtM%252C_%253BiWsLYOhk7RWGeM%252CN98AdZ30Tf_moM%252C_%253BYjx4mdlsPXD_uM%252CImAjdUBN0BOQSM%252C_&usg=AI4_-kRrauMOKdwOnQKws9g4Ff29x9JY-w&sa=X&ved=2ahUKEwi12tvshtD4AhXkxjgGHf6-CJkQ9QF6BAgGEAE#imgrc=PCj5VWGURIThbMnd update the correct campground
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
         if(err){
-           res.redirect("/campgrounds");
+           res.redirect("/houses");
         }else{
             //redirect to the show page
-            res.redirect("/campgrounds/"+ req.params.id);
+            res.redirect("/houses/"+ req.params.id);
         }
     })
 })
 
 //DESTROY CAMPGROUND ROUTE
-router.delete("/campgrounds/:id", middleware.checkCampgroundOwnership, function(req,res){
+router.delete("/houses/:id", middleware.checkCampgroundOwnership, function(req,res){
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
-            res.redirect("/campgrounds");
+            res.redirect("/houses");
         }else{
-            res.redirect("/campgrounds");
+            res.redirect("/houses");
         }
     })
 })
