ce70ce93b3c0f1435c743c3bfe33587a79c71d91
anwesh <pandaanwesh33@gmail.com>
2022-09-01 19:44:50 +0530

    removed flash msg from middleware

--- middleware/index.js
+++ middleware/index.js
@@ -50,7 +50,7 @@ middlewareObj.isLoggedIn = function(req, res, next){
     if(req.isAuthenticated()){
         return next();
     }
-    req.flash('error', 'You must be signed in to do that!'); 
+    // req.flash('error', 'You must be signed in to do that!'); 
     res.redirect("/login");
 }
 
